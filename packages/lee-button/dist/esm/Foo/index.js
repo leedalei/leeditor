d41942cb375a0b7788e74685387a35111381a105
leelei <543784988@qq.com>
2022-08-10 17:12:37 +0800

    upd: first build

---
+++ packages/lee-button/dist/esm/Foo/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { jsxs as _jsxs } from "react/jsx-runtime";
+export default (function (_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/_jsxs("h1", {
+    children: [title, " v2.0"]
+  });
+});
